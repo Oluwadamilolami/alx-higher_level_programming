73c9e54db5a9fc687ad8f9201da58939478848b4
Oluwadamilolami <ojooluwadamilolamary12@gmail.com>
2022-06-09 14:59:34 -0700

    More structure task 103

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -44,6 +44,6 @@ for (i = 0; i < size; i++)
 type = (list->ob_item[i])->ob_type->tp_name;
 printf("Element %i: %s\n", i, type);
 if (!strcmp(type, "bytes"))
-rint_python_bytes(list->ob_item[i]);
+print_python_bytes(list->ob_item[i]);
 }
 }
