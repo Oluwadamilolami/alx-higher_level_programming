62b03e84ba26a9a3a3b1b9b526d0b3d18fddf44f
Oluwadamilolami <ojooluwadamilolamary12@gmail.com>
2022-06-21 20:24:38 -0700

    Python classes and object task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Square Class Module
+"""
+
+
+class Square:
+    """Represents a square.
+    Private instance attribute: size.
+    Instantiation with size (no type/value verification).
+    """
+
+    def __init__(self, size):
+        """Initializes the data."""
+        self.__size = size
